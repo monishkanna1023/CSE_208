4389bb2fa8f28b9185c24cb437c04167c6ce4a1d
monishkanna1023 <59819164+monishkanna1023@users.noreply.github.com>
2020-09-01 22:03:34 +0530

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,17 @@
+public class Test3 {
+    public static void main(String[] args) {
+        Watch_it Now = new Watch_it();
+        Now.TV_Show();
+    }
+    
+}
+
+abstract class bored{
+    abstract void TV_Show();
+}
+
+class Watch_it extends bored{
+    void TV_Show(){
+        System.out.println("Just binge The Simpsons!!!!");
+    }    
+}
