5fafb5755084dbd3a1582b7fce32f11f1b06a2a6
Monish Kanna.R <59819164+monishkanna1023@users.noreply.github.com>
2020-09-02 10:56:44 +0530

    Add files via upload

---
+++ Assignment_1/Q2.java
@@ -0,0 +1,20 @@
+public class Q2 {
+    public static void main(String[] args) {
+        BG_score mine = new BG_score();
+        mine.favorite_score();
+        mine.favorite_song();
+    }
+    
+}
+
+class Music{
+    void favorite_song(){
+        System.out.println("My current favorite song is Marilyn Monroe by Pharell Williams.");
+    }
+}
+
+class BG_score extends Music {
+    void favorite_score(){
+        System.out.println("My current favorite score is FOREVER by Labrinth");
+    }
+}
